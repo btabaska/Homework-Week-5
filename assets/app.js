ab2246eada4deae0f6e15b1d76bc4b3c2546684c
Brandon Tabaska <btabaska@gmail.com>
2020-10-31 15:58:58 -0700

    finished readme and comments

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,8 @@
+//primary object that stores the functions and data needed to build each hour row
 const planObject = {
+  //var to store the current hour
   hour: 0,
+  //getter to grab the current text for each row
   textContent: function () {
     if (localStorage.getItem(this.hour) != undefined) {
       return localStorage.getItem(this.hour);
@@ -7,7 +10,9 @@ const planObject = {
       return "";
     }
   },
+  //determines the difference between the current time and the row time to set the correct CSS
   backgroundColor: function () {
+    //converts time to military time to make the comparison easier
     convertToMiliaryTime = (hour) => {
       if (hour < 9) {
         return hour + 12;
@@ -17,7 +22,7 @@ const planObject = {
     };
     var militaryHour = convertToMiliaryTime(this.hour);
     console.log(militaryHour + " | " + moment().format("H"));
-
+    //returns a string which is then interperated into CSS
     if (moment().format("H") < militaryHour) {
       return "future";
     } else if (moment().format("H") > militaryHour) {
@@ -26,7 +31,7 @@ const planObject = {
       return "present";
     }
   },
-
+  //brute force way of determining if AM or PM should be displayed next to the hour row
   amOrPM: function () {
     if (this.hour <= 5) {
       return "PM";
@@ -34,7 +39,7 @@ const planObject = {
       return "AM";
     }
   },
-
+  //method used to create all the HTML and tags for each of the rows
   divConstructor: function () {
     var hourScreenContainer = document.querySelector(".container");
     var hourScreenDiv = document.createElement("form");
@@ -62,19 +67,20 @@ const planObject = {
   },
 };
 
+//array of the hours that are needed for each row
 var hours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+//for loop goes through and runs a constructor for each of the hours in the array
 hours.forEach((hour) => {
+  //copies the planObject so that it does not mutate the template object
   var localPlanObject = planObject;
+  //sets the copied object hour equal to the object in the array hour
   localPlanObject.hour = hour;
   localPlanObject.divConstructor();
+  //creates an event listener for each object on the save button
   var saveButton = document.querySelector(`#Btn${hour}`);
   saveButton.addEventListener("click", (event) => {
     var userInputValue = document.querySelector(`#textarea${hour}`);
     event.preventDefault();
-    console.log(event.target);
-    console.log(userInputValue);
     localStorage.setItem(hour, userInputValue.value);
   });
 });
-
-console.log(moment().format("h"));
--- assets/timeblock.js
+++ assets/timeblock.js
@@ -1,3 +1,4 @@
+//This is just used to set the current date and time in the header
 var hourScreenContainer = document.querySelector("#currentDay");
 var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 hourScreenContainer.textContent = currentTime;
